b347913226ee5adc30844a2f5d8f120653a80527
user <ryan.denzel@colorado.edu>
2014-12-06 19:13:06 -0700

    encrypts and decrypts

--- pa4-endfs.c
+++ pa4-endfs.c
@@ -72,7 +72,6 @@ static int mirror(char mirDir[PATH], const char *path)
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
 	int res;
-	long unecrsize;
 	char newPath[PATH];
 	mirror(newPath,path);
 
@@ -323,7 +322,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	int fd;
 	int res;
 	FILE *fp, *tmp;
-	
+	int action = 0;
 
 	char newPath[PATH];
 	mirror(newPath, path);
@@ -337,7 +336,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	if (tmp == NULL)
 			return -errno;
 
-	if(!do_crypt(fp, tmp, 0, XMP_INFO->key)){
+	if(!do_crypt(fp, tmp, action, XMP_INFO->key)){
 		fprintf(stderr, "do_crypt failed\n");
     }
     fclose(fp);
